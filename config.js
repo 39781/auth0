3d1687c3949a9df573cb635c62571dae2f19c9d4
Your Name <you@example.com>
2018-06-27 13:09:51 +0530

    fc

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	accessToken:"fb28796ec675402a99b7a97dd04002a1",	
+	dialogFlowAPI:"https://api.api.ai/v1/query?v=20150910",
+	responseObj: {
+	  "payload": {
+		"google": {
+		  "expectUserResponse": true,
+		  "richResponse": {
+			"items": []
+		  }
+		}
+	  }
+	},
+	employees:{
+		"39781":{ph:"8500050085",name:"B+Hari+Prasad%2c"},
+		"39754":{ph:"9626649195",name:"V+Hari+Krishna%2c"}
+	},
+	"smsApi":"http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=ZY2nHm2RiIC&MobileNo=phonenumber&SenderID=TESTIN&Message=Dear+name+the+OTP+to+reset+your+password+is+Otpnumber%2c+valid+only+for+the+next+30++minutes.&ServiceName=TEMPLATE_BASED" 
+}
+
+
+
