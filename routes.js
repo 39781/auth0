28939fb4ad88c7ab65923501706825fd90a147c8
Your Name <you@example.com>
2018-07-03 14:11:07 +0530

    fc

--- routes.js
+++ routes.js
@@ -44,7 +44,7 @@ router.post('/validateUser',function(req, res){
 
 router.get('/redirectUri',function(req,res){
 	//console.log(req.params, req.query, req.url, req);
-	res,end();
+	res.end();
 	//res.redirect('https://logintests.herokuapp.com/redirect.html?empid='+req.query.empId);	
 });
 
