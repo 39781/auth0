39d896fb52a63542b71e0751ea5e6e209ad7a841
Your Name <you@example.com>
2018-07-23 10:47:53 +0530

    fc

--- actions/actions.js
+++ actions/actions.js
@@ -13,8 +13,10 @@ module.exports = {
 			return auth0.generateToken(config.microServicesApis.common, config.appDet.tokenEndPoint)
 			.then(function(result){
 				console.log('result',result);
+				console.log('uid',agent.request_.body.originalDetectIntentRequest.payload.user.userId);
 				loggedUsers[agent.request_.body.originalDetectIntentRequest.payload.user.userId]['access_token'] = result.access_token;
 				agent.setFollowupEvent("gotoMenu");							
+				console.log(loggedUsers[agent.request_.body.originalDetectIntentRequest.payload.user.userId]);
 			})
 			.catch(function(err){
 				agent.add(new Text({'text': `Authentication failed due to some technical issue. Try again later`, 'ssml': `<speak>Hi<break time='5s'/>Authentication failed due to some technical issue. Try again later</speak>` }));				
