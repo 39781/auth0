bcc7cf7ee1f1b5dbc27d8fc791a5c7d4cd89cca9
Your Name <you@example.com>
2018-07-24 09:58:15 +0530

    fc

--- routes.js
+++ routes.js
@@ -289,8 +289,9 @@ router.post('/generateAccessToken',function(req, res){
 		res.json(redirectUrl).end();
 	}else{
 		loggedUsers[params.userId] = params	
-
-		redirectUrl = config.appDet.authorize+'?scope='+config.appDet.scope+'&audience='+config.appDet.audience+'&response_type='+config.appDet.responseType+'&client_id='+config.appDet.clientID+'&redirect_uri='+encodeURIComponent("https://logintests.herokuapp.com/redirectUri?sno=2&empId="+params.empId+"&userId="+params.userId)+'&nonce='+params.access_token+'&prompt=none';
+		
+		
+		redirectUrl = config.appDet.authorize+'?scope='+config.appDet.scope+'&audience='+config.appDet.audience+'&response_type='+config.appDet.responseType+'&client_id='+config.appDet.clientID+'&redirect_uri='+encodeURIComponent("https://logintests.herokuapp.com/redirectUri?sno=2&empId="+params.empId+"&userId="+params.userId)+'&state='+params.access_token+'&prompt=none';
 		
 		console.log(redirectUrl);		
 		res.header('content-type','text/plain');
